9b5e345b1d10ca7fceda86ddf4f6c588b366a662
Ubuntu <ubuntu@ip-172-31-37-242.ec2.internal>
2019-08-05 12:26:05 +0000

    MONGO_URI variable changed

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'the-online-cookbook'
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI", 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
