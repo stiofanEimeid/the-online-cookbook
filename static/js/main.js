dcbb8ead4ed3c9f2110807c8b5ff3d47dcee298c
Ubuntu <ubuntu@ip-172-31-37-242.ec2.internal>
2019-09-05 11:17:11 +0000

    bootstrapious sidebar removed

--- static/js/main.js
+++ static/js/main.js
@@ -90,6 +90,6 @@ $(document).ready(function(){
  
 });
 
-   $(window).on("load",function(){
-         $(".loader-wrapper").fadeOut("slow");
-    });
+//   $(window).on("load",function(){
+//          $(".loader-wrapper").fadeOut("slow");
+//     });
