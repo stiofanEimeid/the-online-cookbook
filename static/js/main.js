e18d7eb5a0365b5526f8e94556f68bde92f2fef2
Ubuntu <ubuntu@ip-172-31-37-242.ec2.internal>
2019-09-06 10:26:23 +0000

    pushing content sideways functionality removed

--- static/js/main.js
+++ static/js/main.js
@@ -57,9 +57,9 @@ $(document).ready(function(){
         $(".overlay").css("display", "block")
         $(".sidebar").css("transform", "translateX(0%)")
         // Push content over
-        $('body').addClass('.overflowHidden')
-        $(".content, .navbar, .footer").removeClass("untranslateMe")
-        $(".content, .navbar, .footer").addClass("translateMe")
+        $(".content, .navbar, .footer").addClass('.overflowHidden')
+        // $(".content, .navbar, .footer").removeClass("untranslateMe")
+        // $(".content, .navbar, .footer").addClass("translateMe")
         
         // $(".content, .navbar, .footer").css("transform", "translateX(250px)")
         
@@ -69,9 +69,9 @@ $(document).ready(function(){
         $(".overlay").css("display", "none")
         $(".sidebar").css("transform", "translateX(-100%)")
         // Push content back
-         $('body').removeClass('.overflowHidden')
-         $(".content, .navbar, .footer").addClass("untranslateMe")
-         $(".content, .navbar, .footer").removeClass("translateMe")
+         $(".content, .navbar, .footer").removeClass('.overflowHidden')
+        //  $(".content, .navbar, .footer").addClass("untranslateMe")
+        //  $(".content, .navbar, .footer").removeClass("translateMe")
          
         // $(".content, .navbar, .footer").css("transform", "translateX(0px)")
       });
