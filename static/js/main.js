416df6c7907994725197a0f2679f850381597f8c
Ubuntu <ubuntu@ip-172-31-37-242.ec2.internal>
2019-09-21 21:48:21 +0000

    js validated

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,105 @@
-// When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar //
-var prevScrollpos = window.pageYOffset;
+/* global dc */
+/* global $ */
+/* global queue */
+/* global d3 */
+/* global crossfilter */
 
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".navbar-wrapper").style.top = "0";
-  } else {
-    document.querySelector(".navbar-wrapper").style.top = "-70px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+$(document).ready(function(){
+    
+$('#myTooltip').tooltip();
+
+$('#mySecondTooltip').tooltip();
+    
+    // Colour group
+    var back = ["#90CCF4", "#f78888", "#f3d250"];
+     
+    $('.card').hover(
+       
+       function(){
+        /* A random colour is chosen from the colour group and set as the 
+         card background on hover */
+         var rand = back[Math.floor(Math.random()*back.length)];
+         $(this).css('background-color', rand);
+           },
+       $(this).css('background-color', '#fff')
+    );
+    
+    // Strike-through property toggled on click for steps and ingredients in recipe page
+    $('.step').click(
+        $(this).toggleClass("strikeMe")
+        );
+        
+    $('.ingredient').click(
+        $(this).toggleClass("strikeMe")
+        );
+      
+    //   overlay only displayed when sidebar selected
+       $(".openbtn" ).click(function(){
+        $(".overlay").css("display", "block");
+        $(".sidebar").css("transform", "translateX(0%)");
+        // Prevents user from scrolling through body while sidenav is open
+        $("body").css('overflow', 'hidden');
+      });
+      
+
+      $(".closebtn").click(function(){
+        $(".overlay").css("display", "none");
+        $(".sidebar").css("transform", "translateX(-100%)");
+        // Allows user to scroll through body after closing sidenav
+         $("body").css('overflow', 'visible');
+      });
+      
+      $(".overlay").click(function(){
+          $(".overlay").css("display", "none");
+        $(".sidebar").css("transform", "translateX(-100%)");
+         // Allows user to scroll through body after closing sidenav
+        $("body").css('overflow', 'visible');
+      });
+
+// https://www.codexworld.com/add-remove-input-fields-dynamically-using-jquery/
+
+    var maxField = 30; //Input fields increment limitation
+    var addButton = $('.add_button'); //Add button selector
+    var wrapper = $('.field_wrapper'); //Input field wrapper
+    var x = 1; //Initial field counter is 1
+    var maxField2 = 30; //Input fields increment limitation
+    var addButton2 = $('.add_button2'); //Add button selector
+    var wrapper2 = $('.field_wrapper2'); //Input field wrapper
+    var y = 1; //Initial field counter is 1
+
+    //Once add button is clicked
+    $(addButton).click(function(){
+        //Check maximum number of input fields
+        if(x < maxField){ 
+            x++; //Increment field counter
+            $(wrapper).append('<div><textarea class="form-control reduce" name="recipe_ingredient" rows="1" value="" placeholder="Ingredient"></textarea><a href="#" class="remove_button">&#10008;</a></div>'); //Add field html
+          
+        }
+    });
+    
+    //Once remove button is clicked
+    $(wrapper).on('click', '.remove_button', function(e){
+        e.preventDefault();
+        $(this).parent('div').remove(); //Remove field html
+        x--; //Decrement field counter
+    });
+
+    //Once add button is clicked
+    $(addButton2).click(function(){
+        //Check maximum number of input fields
+        if(y < maxField2){ 
+            y++; //Increment field counter
+            $(wrapper2).append('<div><textarea class="form-control reduce" name="recipe_step" rows="2" value="" placeholder="Step"/><a href="#" class="remove_button2">&#10008;</a></div>'); //Add field html
+        }
+    });
+    
+    //Once remove button is clicked
+    $(wrapper2).on('click', '.remove_button2', function(e){
+        e.preventDefault();
+        $(this).parent('div').remove(); //Remove field html
+        x--; //Decrement field counter
+    });
+});
 
 // Credit: https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
     queue()
@@ -80,103 +170,15 @@ window.onscroll = function() {
         dc.renderAll();
 }
 
+// When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar //
+var prevScrollpos = window.pageYOffset;
 
-  
-$(document).ready(function(){
-    
-$('#myTooltip').tooltip();
-
-$('#mySecondTooltip').tooltip();
-    
-    // Colour group
-    var back = ["#90CCF4", "#f78888", "#f3d250"];
-     
-    $('.card').hover(
-       
-       function(){
-        /* A random colour is chosen from the colour group and set as the 
-         card background on hover */
-           var rand = back[Math.floor(Math.random()*back.length)];
-           $(this).css('background-color', rand) },
-       function(){ $(this).css('background-color', '#fff') }
-    );
-    
-    // Strike-through property toggled on click for steps and ingredients in recipe page
-    $('.step').click(
-        function(){ $(this).toggleClass("strikeMe")},
-        );
-    $('.ingredient').click(
-        function(){ $(this).toggleClass("strikeMe")},
-        );
-      
-    //   overlay only displayed when sidebar selected
-       $(".openbtn" ).click(function(){
-        $(".overlay").css("display", "block")
-        $(".sidebar").css("transform", "translateX(0%)")
-        // Prevents user from scrolling through body while sidenav is open
-        $("body").css('overflow', 'hidden')
-      });
-      
-
-      $(".closebtn").click(function(){
-        $(".overlay").css("display", "none")
-        $(".sidebar").css("transform", "translateX(-100%)")
-        // Allows user to scroll through body after closing sidenav
-         $("body").css('overflow', 'visible')
-     
-      });
-      
-      $(".overlay").click(function(){
-          $(".overlay").css("display", "none")
-        $(".sidebar").css("transform", "translateX(-100%)")
-         // Allows user to scroll through body after closing sidenav
-        $("body").css('overflow', 'visible')
-         
-       
-      });
-
-// https://www.codexworld.com/add-remove-input-fields-dynamically-using-jquery/
-
-    var maxField = 30; //Input fields increment limitation
-    var addButton = $('.add_button'); //Add button selector
-    var wrapper = $('.field_wrapper'); //Input field wrapper
-    var x = 1; //Initial field counter is 1
-    var maxField2 = 30; //Input fields increment limitation
-    var addButton2 = $('.add_button2'); //Add button selector
-    var wrapper2 = $('.field_wrapper2'); //Input field wrapper
-    var y = 1; //Initial field counter is 1
-
-    //Once add button is clicked
-    $(addButton).click(function(){
-        //Check maximum number of input fields
-        if(x < maxField){ 
-            x++; //Increment field counter
-            $(wrapper).append('<div><textarea class="form-control reduce" name="recipe_ingredient" rows="1" value="" placeholder="Ingredient"></textarea><a href="#" class="remove_button">&#10008;</a></div>'); //Add field html
-          
-        }
-    });
-    
-    //Once remove button is clicked
-    $(wrapper).on('click', '.remove_button', function(e){
-        e.preventDefault();
-        $(this).parent('div').remove(); //Remove field html
-        x--; //Decrement field counter
-    });
-
-    //Once add button is clicked
-    $(addButton2).click(function(){
-        //Check maximum number of input fields
-        if(y < maxField2){ 
-            y++; //Increment field counter
-            $(wrapper2).append('<div><textarea class="form-control reduce" name="recipe_step" rows="2" value="" placeholder="Step"/><a href="#" class="remove_button2">&#10008;</a></div>'); //Add field html
-        }
-    });
-    
-    //Once remove button is clicked
-    $(wrapper2).on('click', '.remove_button2', function(e){
-        e.preventDefault();
-        $(this).parent('div').remove(); //Remove field html
-        x--; //Decrement field counter
-    });
-});
-
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navbar-wrapper").style.top = "0";
+  } else {
+    document.querySelector(".navbar-wrapper").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+};
