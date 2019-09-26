2d9119538d931ee738a0834fbff4f15b28084a1d
Ubuntu <ubuntu@ip-172-31-37-242.ec2.internal>
2019-09-26 13:15:02 +0000

    percentage cutoff point in charts changed from 5 to 7

--- static/js/main.js
+++ static/js/main.js
@@ -131,7 +131,7 @@ function myTag(key, endAngle, startAngle){
     var percentage = dc.utils.printSingleValue((endAngle - startAngle)
     / (2*Math.PI) * 100);
     // hide tag is less than 5% of the chart to prevent labels from overlapping
-    if (percentage >= 5){
+    if (percentage >= 7){
         return key + "(" + Math.round(percentage) + '%)';
         }
     }
